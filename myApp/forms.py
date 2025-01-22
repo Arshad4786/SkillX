f9052da3c611d6722d99575c3603c81eb7b3a636
Arshad4786 <arshad88908@gmail.com>
2025-01-22 22:17:15 +0530

    added styling

--- myApp/forms.py
+++ myApp/forms.py
@@ -5,3 +5,12 @@ class TalentForm(forms.ModelForm):
     class Meta:
         model = Talent
         fields = ['name', 'email', 'phone', 'skills', 'description', 'profile_photo']
+        
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'input-field', 'placeholder': 'Your Full Name'}),
+            'email': forms.EmailInput(attrs={'class': 'input-field', 'placeholder': 'Your Email Address'}),
+            'phone': forms.TextInput(attrs={'class': 'input-field', 'placeholder': 'Your Phone Number'}),
+            'skills': forms.Textarea(attrs={'class': 'input-field', 'placeholder': 'Your Skills and Expertise', 'rows': 4}),
+            'description': forms.Textarea(attrs={'class': 'input-field', 'placeholder': 'A brief description about you', 'rows': 4}),
+            'profile_photo': forms.ClearableFileInput(attrs={'class': 'input-field'}),
+        }
